365f01d1f70df6488316ddb9d1dcd060cfd8ffda
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2024-01-29 19:17:50 -0600

    2024-01-29 v3

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,9 @@
-"use server";
 import { authOptions } from "@/server/auth";
 import { type NextApiRequest, type NextApiResponse } from "next";
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await NextAuth(req, res, authOptions);
+  return (await NextAuth(req, res, authOptions)) as NextAuthOptions;
 }
 
 export { handler as GET, handler as POST };
